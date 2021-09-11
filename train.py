43f93babb6cc90feb263336996bcd052135383a3
Gonzalo Melendi <gmelendi@hotmail.com>
2021-09-11 16:25:35 +0200

    no augmentation

--- train.py
+++ train.py
@@ -93,62 +93,62 @@ test_ds = datagen_eval.flow_from_dataframe(
 # data augmentation 
 # ==========================
 
-def prepare_jpg(df_data):
-    files = (df_data['dir']+'/'+df_data['file']).tolist()
-    labels = tf.one_hot((df_data['class'].astype(int)).tolist(), num_classes)
-    return np.array([np.asarray(Image.open(fname)) for fname in files]), labels
+# def prepare_jpg(df_data):
+#     files = (df_data['dir']+'/'+df_data['file']).tolist()
+#     labels = tf.one_hot((df_data['class'].astype(int)).tolist(), num_classes)
+#     return np.array([np.asarray(Image.open(fname)) for fname in files]), labels
 
 
-trash_data = train_data.loc[train_data.label=='trash']
-data_trash, trash_labels = prepare_jpg(trash_data)
-trash_ds = tf.data.Dataset.from_tensor_slices((data_trash, trash_labels)).batch(32)
+# trash_data = train_data.loc[train_data.label=='trash']
+# data_trash, trash_labels = prepare_jpg(trash_data)
+# trash_ds = tf.data.Dataset.from_tensor_slices((data_trash, trash_labels)).batch(32)
 
-aug_dir = 'aug_images'
-if not os.path.isdir(aug_dir):
-    os.makedirs(aug_dir)
+# aug_dir = 'aug_images'
+# if not os.path.isdir(aug_dir):
+#     os.makedirs(aug_dir)
 
-for file in os.listdir(aug_dir):
-    os.remove(os.path.join(aug_dir, file))
+# for file in os.listdir(aug_dir):
+#     os.remove(os.path.join(aug_dir, file))
     
-aug_factor = train_data['class'].value_counts().max() - train_data['class'].value_counts().min()
-for x, y in trash_ds:
-    gen = tf.keras.preprocessing.image.ImageDataGenerator(
-        zoom_range=0.3,
-        horizontal_flip=True,
-        rotation_range=90,
-        width_shift_range=.2,
-        height_shift_range=.2,
-        brightness_range=(0.25, 0.75)
-    )
-    gen.fit(x)
-    for x in gen.flow(x, batch_size=32, save_to_dir=aug_dir, save_prefix='aug', save_format='jpg'):
-        if len(os.listdir(aug_dir)) >= aug_factor:
-            print('generated %d augmented images'%(len(os.listdir(aug_dir))))
-            break
-    break
+# aug_factor = train_data['class'].value_counts().max() - train_data['class'].value_counts().min()
+# for x, y in trash_ds:
+#     gen = tf.keras.preprocessing.image.ImageDataGenerator(
+#         zoom_range=0.3,
+#         horizontal_flip=True,
+#         rotation_range=90,
+#         width_shift_range=.2,
+#         height_shift_range=.2,
+#         brightness_range=(0.25, 0.75)
+#     )
+#     gen.fit(x)
+#     for x in gen.flow(x, batch_size=32, save_to_dir=aug_dir, save_prefix='aug', save_format='jpg'):
+#         if len(os.listdir(aug_dir)) >= aug_factor:
+#             print('generated %d augmented images'%(len(os.listdir(aug_dir))))
+#             break
+#     break
 
 
-trash_df = []
-for r, d, f in os.walk(aug_dir):
-    for file in f:
-        trash_df.append({'dir': r, 'file': file, 'path': os.path.join(r, file)})
+# trash_df = []
+# for r, d, f in os.walk(aug_dir):
+#     for file in f:
+#         trash_df.append({'dir': r, 'file': file, 'path': os.path.join(r, file)})
         
-trash_df = pd.DataFrame(trash_df).sample(n=aug_factor)
-trash_df['label'] = 'trash'
-trash_df['class'] = str(classes.index('trash'))
+# trash_df = pd.DataFrame(trash_df).sample(n=aug_factor)
+# trash_df['label'] = 'trash'
+# trash_df['class'] = str(classes.index('trash'))
 
-train_data = pd.concat([train_data, trash_df], axis=0)
-# append to train_ds
-train_ds = datagen_train.flow_from_dataframe(
-    train_data,  
-    x_col='path', 
-    y_col='class',
-    class_mode='categorical',
-    color_mode='rgb',
-    batch_size=32, 
-    shuffle=True,
-    save_format='jpg',
-)
+# train_data = pd.concat([train_data, trash_df], axis=0)
+# # append to train_ds
+# train_ds = datagen_train.flow_from_dataframe(
+#     train_data,  
+#     x_col='path', 
+#     y_col='class',
+#     class_mode='categorical',
+#     color_mode='rgb',
+#     batch_size=32, 
+#     shuffle=True,
+#     save_format='jpg',
+# )
 
 # =======================
 # model arquitecture
@@ -196,13 +196,15 @@ class_weights = hist.min()/hist
 class_weights = {i:class_weights[i] for i in range(num_classes)}
 print(class_weights)
 
-models_dir = 'models/checkpoint'
-if not os.path.isdir(models_dir): os.makedirs(models_dir)
+model_dir = os.path.join('models', datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+chkpt_dir = os.path.join(model_dir, 'checkpoint')
+
+if not os.path.isdir(chkpt_dir): os.makedirs(chkpt_dir)
 
 callbacks = [
     tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True, verbose=1),
     # tf.keras.callbacks.ReduceLROnPlateau(monitor='val_categorical_accuracy', patience=8, min_lr=1e-6, factor=0.1),
-    tf.keras.callbacks.ModelCheckpoint(models_dir, monitor='val_categorical_accuracy', save_best_only=True)
+    tf.keras.callbacks.ModelCheckpoint(chkpt_dir, monitor='val_categorical_accuracy', save_best_only=True)
 ]
 
 epochs = 100
@@ -237,3 +239,9 @@ conf_mat = confusion_matrix(test_true, test_pred, normalize='true')
 conf_map = pd.DataFrame(columns=classes, data=conf_mat, index=classes)
 conf_map
 sns.heatmap(conf_mat, annot=True, xticklabels=classes, yticklabels=classes, cmap="YlGnBu", cbar=False);
+
+# ==================
+# save trained model
+# ==================
+save_dir = os.path.join(model_dir, 'best_model')
+tf.keras.models.save_model(model, save_dir)
