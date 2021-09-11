3e4e4c7ac85cd8f048e9e533fcd7007424852311
Gonzalo Melendi <gmelendi@hotmail.com>
2021-09-11 14:01:39 +0200

    new train with augmented images

--- train.py
+++ train.py
@@ -34,9 +34,6 @@ train_data = data.merge(load_data('data/zero-indexed-files.txt'), on=['file'], h
 train_data, val_data = train_test_split(train_data, test_size=0.15)
 train_data, test_data = train_test_split(train_data, test_size=0.15)
 
-# val_data = data.merge(load_data('data/zero-indexed-files-notrash_val.txt'), on=['file'], how='inner').sample(frac=1)
-# test_data = data.merge(load_data('data/one-indexed-files-notrash_test.txt'), on=['file'], how='inner').sample(frac=1)
-
 
 num_classes = train_data['class'].nunique()
 classes = train_data[['class', 'label']].drop_duplicates().sort_values('class')['label'].tolist()
@@ -47,7 +44,6 @@ res_img_width, res_img_height = 299, 299
 
 
 datagen_train = tf.keras.preprocessing.image.ImageDataGenerator(
-        # rescale=1./255,
         shear_range=0.1,
         zoom_range=0.2,
         horizontal_flip=True,
@@ -70,7 +66,6 @@ train_ds = datagen_train.flow_from_dataframe(
 
 
 datagen_eval = tf.keras.preprocessing.image.ImageDataGenerator(
-        # rescale=1./255,
         preprocessing_function=preprocess_input
 )
 
@@ -103,65 +98,63 @@ test_ds = datagen_eval.flow_from_dataframe(
 # data augmentation 
 # ==========================
 
-# def prepare_jpg(df_data):
-#     files = (df_data['dir']+'/'+df_data['file']).tolist()
-#     labels = tf.one_hot((df_data['class'].astype(int)).tolist(), num_classes)
-#     return np.array([np.asarray(Image.open(fname)) for fname in files]), labels
+def prepare_jpg(df_data):
+    files = (df_data['dir']+'/'+df_data['file']).tolist()
+    labels = tf.one_hot((df_data['class'].astype(int)).tolist(), num_classes)
+    return np.array([np.asarray(Image.open(fname)) for fname in files]), labels
 
 
-# trash_data = train_data.loc[train_data.label=='trash']
-# data_trash, trash_labels = prepare_jpg(trash_data)
-# trash_ds = tf.data.Dataset.from_tensor_slices((data_trash, trash_labels)).batch(32)
+trash_data = train_data.loc[train_data.label=='trash']
+data_trash, trash_labels = prepare_jpg(trash_data)
+trash_ds = tf.data.Dataset.from_tensor_slices((data_trash, trash_labels)).batch(32)
 
-# aug_dir = 'aug_images'
-# if not os.path.isdir(aug_dir):
-#     os.makedirs(aug_dir)
+aug_dir = 'aug_images'
+if not os.path.isdir(aug_dir):
+    os.makedirs(aug_dir)
 
-# for file in os.listdir(aug_dir):
-#     os.remove(os.path.join(aug_dir, file))
+for file in os.listdir(aug_dir):
+    os.remove(os.path.join(aug_dir, file))
     
-# aug_factor = train_data['class'].value_counts().max() - train_data['class'].value_counts().min()
-# for x, y in trash_ds:
-#     gen = tf.keras.preprocessing.image.ImageDataGenerator(
-#         zoom_range=0.3,
-#         horizontal_flip=True,
-#         rotation_range=90,
-#         width_shift_range=.2,
-#         height_shift_range=.2,
-#         brightness_range=(0.25, 0.75)
-#     )
-#     gen.fit(x)
-#     for x in gen.flow(x, batch_size=32, save_to_dir=aug_dir, save_prefix='aug', save_format='jpg'):
-#         if len(os.listdir(aug_dir)) >= aug_factor:
-#             print('generated %d augmented images'%(len(os.listdir(aug_dir))))
-#             break
-#     break
+aug_factor = train_data['class'].value_counts().max() - train_data['class'].value_counts().min()
+for x, y in trash_ds:
+    gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        zoom_range=0.3,
+        horizontal_flip=True,
+        rotation_range=90,
+        width_shift_range=.2,
+        height_shift_range=.2,
+        brightness_range=(0.25, 0.75)
+    )
+    gen.fit(x)
+    for x in gen.flow(x, batch_size=32, save_to_dir=aug_dir, save_prefix='aug', save_format='jpg'):
+        if len(os.listdir(aug_dir)) >= aug_factor:
+            print('generated %d augmented images'%(len(os.listdir(aug_dir))))
+            break
+    break
 
 
-# trash_df = []
-# for r, d, f in os.walk('aug_images'):
-#     for file in f:
-#         trash_df.append({'dir': r, 'file': file, 'path': os.path.join(r, file)})
+trash_df = []
+for r, d, f in os.walk(aug_dir):
+    for file in f:
+        trash_df.append({'dir': r, 'file': file, 'path': os.path.join(r, file)})
         
-# trash_df = pd.DataFrame(trash_df).sample(n=aug_factor)
-# trash_df['label'] = 'trash'
-# trash_df['class'] = str(classes.index('trash'))
-# # data_trash, trash_labels = prepare_jpg(trash_df)
-# # trash_ds = tf.data.Dataset.from_tensor_slices((data_trash, trash_labels)).batch(32)
+trash_df = pd.DataFrame(trash_df).sample(n=aug_factor)
+trash_df['label'] = 'trash'
+trash_df['class'] = str(classes.index('trash'))
 
-# train_data = pd.concat([train_data, trash_df], axis=0)
-# # append to train_ds
-# train_ds = datagen.flow_from_dataframe(
-#     train_data,  
-#     x_col='path', 
-#     y_col='class',
-#     class_mode='categorical',
-#     target_size=(res_img_width, res_img_height), 
-#     color_mode='rgb',
-#     batch_size=32, 
-#     shuffle=True,
-#     save_format='jpg',
-# )
+train_data = pd.concat([train_data, trash_df], axis=0)
+# append to train_ds
+train_ds = datagen_train.flow_from_dataframe(
+    train_data,  
+    x_col='path', 
+    y_col='class',
+    class_mode='categorical',
+    target_size=(res_img_width, res_img_height), 
+    color_mode='rgb',
+    batch_size=32, 
+    shuffle=True,
+    save_format='jpg',
+)
 
 # =======================
 # model arquitecture
